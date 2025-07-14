c0eb8d7ad3408f007fb4febc939d5066fda3754b
weiihann <weihan774237@gmail.com>
2025-07-14 09:56:12 +0800

    make unmarshaling more robust

--- pkg/rpc/client.go
+++ pkg/rpc/client.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"context"
+	"encoding/json"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -62,10 +63,75 @@ type DiffItem struct {
 
 // AccountDiff represents the changes to a single account
 type AccountDiff struct {
-	Balance any `json:"balance,omitempty"`
-	Code    any `json:"code,omitempty"`
-	Nonce   any `json:"nonce,omitempty"`
-	Storage any `json:"storage,omitempty"`
+	Balance        any  `json:"balance,omitempty"`
+	Code           any  `json:"code,omitempty"`
+	Nonce          any  `json:"nonce,omitempty"`
+	Storage        any  `json:"storage,omitempty"`
+	AccountChanged bool `json:"-"`
+	StorageChanged bool `json:"-"`
+}
+
+// UnmarshalJSON custom unmarshaller for AccountDiff to set change flags
+func (ad *AccountDiff) UnmarshalJSON(data []byte) error {
+	var raw map[string]json.RawMessage
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	// Helper function to process balance/code/nonce
+	processField := func(field json.RawMessage, target *any) bool {
+		var s string
+		if json.Unmarshal(field, &s) == nil && s == "=" {
+			*target = s
+			return false
+		}
+		var m map[string]DiffItem
+		if json.Unmarshal(field, &m) == nil {
+			*target = m
+			return true
+		}
+		// If neither, set to raw message
+		*target = field
+		return false
+	}
+
+	// Process balance
+	if bal, ok := raw["balance"]; ok {
+		if processField(bal, &ad.Balance) {
+			ad.AccountChanged = true
+		}
+	}
+
+	// Process code
+	if code, ok := raw["code"]; ok {
+		if processField(code, &ad.Code) {
+			ad.AccountChanged = true
+		}
+	}
+
+	// Process nonce
+	if nonce, ok := raw["nonce"]; ok {
+		if processField(nonce, &ad.Nonce) {
+			ad.AccountChanged = true
+		}
+	}
+
+	// Process storage
+	if stor, ok := raw["storage"]; ok {
+		var m map[string]map[string]DiffItem
+		if json.Unmarshal(stor, &m) == nil {
+			ad.Storage = m
+			if len(m) > 0 {
+				ad.StorageChanged = true
+				ad.AccountChanged = true
+			}
+		} else {
+			// If not map, set to raw
+			ad.Storage = stor
+		}
+	}
+
+	return nil
 }
 
 // StateDiff represents the state diff for a single transaction
